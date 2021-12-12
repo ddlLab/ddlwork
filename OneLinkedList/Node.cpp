3ac9282f2c51d40607127e2df0593ce5cc08c3ce
Oleg2604 <olegmart2604@gmail.com>
2021-12-12 14:32:56 +0300

    *OneLinkedList 1

---
+++ OneLinkedList/Node.cpp
@@ -0,0 +1,18 @@
+#include "node.h"
+#include<sstream>
+
+Node::Node(int _data, Node* _next)
+: data(_data)
+, next(_next)
+{}
+
+std::string Node::ToString() const
+{
+	std::ostringstream oss;
+	oss << data << " ";
+	if (!IsLast())
+	{
+		oss << next->ToString();
+	}
+	return oss.str();
+}
---
+++ OneLinkedList/Node.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class Node
+{
+public:
+	Node(int data, Node* next = nullptr);
+
+	Node* Next()             { return next; }
+	const Node* Next() const { return next; }
+	
+	int& Data()              { return data; }
+	const int& Data() const { return data; }
+
+	bool IsLast() const { return next == nullptr; }
+	std::string ToString() const;
+private:
+	Node* next = nullptr;
+	int data = 0;
+};
