08796ae8b0e16620c6817736ba72651fdc18a4bc
ddlLab <ddl.rybchenko@gmail.com>
2021-12-12 13:31:30 +0200

    *first steps to create oll

---
+++ OneLinkedList/one_linked_list.cpp
@@ -0,0 +1,29 @@
+#include "one_linked_list.h"
+#include "node.h"
+#include <sstream>
+
+std::string OneLinkedList::ToString() const
+{
+	std::ostringstream oss;
+	oss << size << ":[";
+	if (head)
+	{
+		oss << head->ToString();
+	}
+	oss << "]";
+	return oss.str();
+}
+
+void OneLinkedList::push_front(int val)
+{
+	if (size == 0)
+	{
+		head = new Node(val);
+		tail = head;
+	}
+	else
+	{
+		head = new Node(val, head);
+	}
+	size++;
+}
---
+++ OneLinkedList/one_linked_list.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+class Node;
+
+class OneLinkedList
+{
+public:
+	size_t Size() const { return size; }
+	std::string ToString() const;
+
+	void push_front(int val);
+private:
+	Node* head = nullptr;
+	Node* tail = nullptr;
+	size_t size = 0;
+};
