488d57a4f1d84727f3f07603eb970ad6f396fb48
nikoeyren <nikita.godunov.666@gmail.com>
2021-12-12 13:31:58 +0200

    class work

---
+++ OneLinkedList/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "one_linked_list.h"
+
+int main()
+{
+	OneLinkedList list;
+	std::cout << list.ToString() << std::endl;
+	list.push_front(10);
+	std::cout << list.ToString() << std::endl;
+	list.push_front(11);
+	std::cout << list.ToString() << std::endl;
+	list.push_front(12);
+	std::cout << list.ToString() << std::endl;
+	list.push_front(13);
+	std::cout << list.ToString() << std::endl;
+	list.push_front(14);
+	std::cout << list.ToString() << std::endl;
+	list.push_front(15);
+	std::cout << list.ToString() << std::endl;
+	return 0;
+}
