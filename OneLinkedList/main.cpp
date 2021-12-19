2370cda26e61f0ea993d59358958d7a936985afc
M4XD1L4N <0max.dilan0@gmail.com>
2021-12-19 13:35:15 +0200

    Renat Error

--- OneLinkedList/main.cpp
+++ OneLinkedList/main.cpp
@@ -9,17 +9,30 @@ int main()
 	OneLinkedList list;
 
 	cout << list.Tostring() << endl;
-	list.push_front(10);
+	list.push_back(10);
 	cout << list.Tostring() << endl;
-	list.push_front(11);
+	list.push_back(11);
 	cout << list.Tostring() << endl;
-	list.push_front(12);
+	list.push_back(12);
 	cout << list.Tostring() << endl;
-	list.push_front(13);
+	list.push_back(13);
 	cout << list.Tostring() << endl;
-	list.push_front(14);
+	list.push_back(14);
 	cout << list.Tostring() << endl;
-	list.push_front(15);
+	list.push_back(15);
 	cout << list.Tostring() << endl;
+
+	list.push_mid(21,0);
+	cout << list.Tostring() << endl;
+	list.push_mid(22,7);
+	cout << list.Tostring() << endl;
+	list.push_mid(23,3);
+	cout << list.Tostring() << endl;
+
+	while (list.Size() > 0)
+	{
+		list.pop_back();
+		cout << list.Tostring() << endl;
+	}
 	return 0;
 }
--- OneLinkedList/one_linked_list.h
+++ OneLinkedList/one_linked_list.h
@@ -8,10 +8,16 @@ class Node;
 class OneLinkedList
 {
 public:
-	size_t* Size() const { return nullptr; }
+	size_t Size() const { return size; }
 	string Tostring() const;
 
 	void push_front(int val);
+	void push_back(int val);
+	void push_mid(int val, size_t pos);
+
+	void pop_front();
+	void pop_back();
+
 private:
 	Node* head = nullptr;
 	Node* tail = nullptr;
