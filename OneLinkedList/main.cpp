03393e0961b71bb8b49698d6f558be6fd9250725
Darthiuss <yehor.danyliuk@ukr.net>
2021-12-19 13:43:19 +0200

    classwork

--- OneLinkedList/main.cpp
+++ OneLinkedList/main.cpp
@@ -5,17 +5,29 @@ int main()
 {
 	OneLinkedList list;
 	cout << list.ToString() << endl;
-	list.push_front(10);
+	list.push_back(10);
 	cout << list.ToString() << endl;
-	list.push_front(11);
+	list.push_back(11);
 	cout << list.ToString() << endl;
-	list.push_front(12);
+	list.push_back(12);
 	cout << list.ToString() << endl;
-	list.push_front(13);
+	list.push_back(13);
 	cout << list.ToString() << endl;
-	list.push_front(14);
+	list.push_back(14);
 	cout << list.ToString() << endl;
-	list.push_front(15);
+	list.push_back(15);
 	cout << list.ToString() << endl;
+
+	list.push_mid(21, 0);
+	cout << list.ToString() << endl;
+	list.push_mid(22, 15);
+	cout << list.ToString() << endl;
+	list.push_mid(23, 3);
+	cout << list.ToString() << endl;
+	while (list.Size() > 0)
+	{
+		list.pop_back();
+		cout << list.ToString() << endl;
+	}
 	return 0;
 }
