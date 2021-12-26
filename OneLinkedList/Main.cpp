8d6ded972f82a2d6feca704a6b202c82976526dd
nikoeyren <nikita.godunov.666@gmail.com>
2021-12-26 13:50:43 +0200

    class work with some errors

--- OneLinkedList/Main.cpp
+++ OneLinkedList/Main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "one_linked_list.h"
+#include "book.h"
+
+void test1();
+void test2();
+void test3();
+
 
 int main()
 {
-	OneLinkedList list;
+	test1();
+	test2();
+	test3();
+	return 0;
+}
+
+
+void test1()
+{
+	OneLinkedList<int> list;
 	std::cout << list.ToString() << std::endl;
 	list.push_back(10);
 	std::cout << list.ToString() << std::endl;
@@ -21,11 +36,66 @@ int main()
 	list.push_mid(6, 0);
 	list.push_mid(7, 15);
 	std::cout << list.ToString() << std::endl;
-	
+
 	while (!list.IsEmpty())
 	{
-		list.pop_back();
+		list.pop_mid(3);
 		std::cout << list.ToString() << std::endl;
 	}
-	return 0;
+}
+void test2()
+{
+	OneLinkedList<float> list;
+	std::cout << list.ToString() << std::endl;
+	list.push_back(10.1);
+	std::cout << list.ToString() << std::endl;
+	list.push_back(11.2);
+	std::cout << list.ToString() << std::endl;
+	list.push_back(12.3);
+	std::cout << list.ToString() << std::endl;
+	list.push_back(13.4);
+	std::cout << list.ToString() << std::endl;
+	list.push_back(14.5);
+	std::cout << list.ToString() << std::endl;
+	list.push_back(15.6);
+	std::cout << list.ToString() << std::endl;
+	list.push_mid(5.7, 3);
+	list.push_mid(6.8, 0);
+	list.push_mid(7.9, 15);
+	std::cout << list.ToString() << std::endl;
+
+	while (!list.IsEmpty())
+	{
+		list.pop_mid(3);
+		std::cout << list.ToString() << std::endl;
+	}
+}
+
+
+void test3()
+{
+	OneLinkedList<Book> list;
+	std::cout << list.ToString() << std::endl;
+	list.push_back({ "Lev Tolstoy1", 5, 10 });
+	std::cout << list.ToString() << std::endl;
+	list.push_back({ "Lev Tolstoy2", 6, 11 });
+	std::cout << list.ToString() << std::endl;
+	list.push_back({ "Lev Tolstoy3", 7, 15 });
+	std::cout << list.ToString() << std::endl;
+	list.push_back({ "Lev Tolstoy4", 8, 16 });
+	std::cout << list.ToString() << std::endl;
+	list.push_back({ "Lev Tolstoy5", 9, 13 });
+	std::cout << list.ToString() << std::endl;
+	list.push_back({ "Lev Tolstoy6", 1, 18 });
+	std::cout << list.ToString() << std::endl;
+	list.push_mid({ "Lev Tolstoy7", 2, 12 });
+	list.push_mid({ "Lev Tolstoy8", 3, 13 });
+	list.push_mid({ "Lev Tolstoy9", 4, 7 });
+	std::cout << list.ToString() << std::endl;
+
+	while (!list.IsEmpty())
+	{
+		list.pop_mid(3);
+		std::cout << list.ToString() << std::endl;
+	}
 }
