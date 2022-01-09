b4690f8d1e376d0e2a471b6db650c910015dbfb8
ddlLab <ddl.rybchenko@gmail.com>
2022-01-09 12:12:02 +0200

    *create oll template

---
+++ OneLinkedList/Book.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include<string>
+#include<sstream>
+
+class Book
+{
+public:
+	Book(std::string _autor, int _tiraz, float _cost)
+	: autor(_autor)
+	, cost(_cost)
+	, tiraz(_tiraz)
+	{}
+
+	std::string ToString() const
+	{
+		std::ostringstream oss;
+		oss << "{" << autor << "," << cost << "," << tiraz << "}";
+		return oss.str();
+	}
+
+	float cost = 0;
+	int tiraz = 0;
+	std::string autor;
+};
+
+std::ostream& operator<<(std::ostream& os, const Book& book);
+
--- OneLinkedList/main.cpp
+++ OneLinkedList/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include "one_linked_list.h"
+#include "Book.h"
+
+void test1();
+void test2();
+void test3();
 
 int main()
 {
-    OneLinkedList list;
-    std::cout << list.ToString() <<std::endl;
+    test1();
+    test2();
+    test3();
+    return 0;
+}
+
+void test1()
+{
+    OneLinkedList<int> list;
+    std::cout << list.ToString() << std::endl;
     list.push_back(10);
-    std::cout << list.ToString() <<std::endl;
+    std::cout << list.ToString() << std::endl;
     list.push_back(11);
-    std::cout << list.ToString() <<std::endl;
+    std::cout << list.ToString() << std::endl;
     list.push_back(12);
-    std::cout << list.ToString() <<std::endl;
+    std::cout << list.ToString() << std::endl;
     list.push_back(13);
-    std::cout << list.ToString() <<std::endl;
+    std::cout << list.ToString() << std::endl;
     list.push_back(14);
-    std::cout << list.ToString() <<std::endl;
+    std::cout << list.ToString() << std::endl;
     list.push_back(15);
-    std::cout << list.ToString() <<std::endl;
+    std::cout << list.ToString() << std::endl;
 
     list.push_mid(21, 0);
     std::cout << list.ToString() << std::endl;
@@ -26,9 +39,69 @@ int main()
     std::cout << list.ToString() << std::endl;
     while (list.Size() > 0)
     {
-        list.pop_back();
+        list.pop_mid(3);
         std::cout << list.ToString() << std::endl;
     }
+}
 
-    return 0;
+void test2()
+{
+    OneLinkedList<float> list;
+    std::cout << list.ToString() << std::endl;
+    list.push_back(10.2);
+    std::cout << list.ToString() << std::endl;
+    list.push_back(11.3);
+    std::cout << list.ToString() << std::endl;
+    list.push_back(12.5);
+    std::cout << list.ToString() << std::endl;
+    list.push_back(13.1);
+    std::cout << list.ToString() << std::endl;
+    list.push_back(14.4);
+    std::cout << list.ToString() << std::endl;
+    list.push_back(15.6);
+    std::cout << list.ToString() << std::endl;
+
+    list.push_mid(21.7, 0);
+    std::cout << list.ToString() << std::endl;
+    list.push_mid(22.8, 15);
+    std::cout << list.ToString() << std::endl;
+    list.push_mid(23.9, 3);
+    std::cout << list.ToString() << std::endl;
+    while (list.Size() > 0)
+    {
+        list.pop_mid(3);
+        std::cout << list.ToString() << std::endl;
+    }
+}
+
+
+
+void test3()
+{
+    OneLinkedList<Book> list;
+    std::cout << list.ToString() << std::endl;
+    list.push_back({"test1",100, 12.5});
+    std::cout << list.ToString() << std::endl;
+    list.push_back({ "test2",90, 12.5 });
+    std::cout << list.ToString() << std::endl;
+    list.push_back({ "test3",80, 12.5 });
+    std::cout << list.ToString() << std::endl;
+    list.push_back({ "test4",70, 12.5 });
+    std::cout << list.ToString() << std::endl;
+    list.push_back({ "test5",60, 12.5 });
+    std::cout << list.ToString() << std::endl;
+    list.push_back({ "test6",50, 12.5 });
+    std::cout << list.ToString() << std::endl;
+
+    list.push_mid({ "test7",40, 12.5 }, 0);
+    std::cout << list.ToString() << std::endl;
+    list.push_mid({ "test8",30, 12.5 }, 15);
+    std::cout << list.ToString() << std::endl;
+    list.push_mid({ "test9",20, 12.5 }, 3);
+    std::cout << list.ToString() << std::endl;
+    while (list.Size() > 0)
+    {
+        list.pop_mid(3);
+        std::cout << list.ToString() << std::endl;
+    }
 }
