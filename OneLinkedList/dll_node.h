846eda566d6cde57d2eaff419ba170594cc3f63c
M4XD1L4N <0max.dilan0@gmail.com>
2022-01-16 13:36:56 +0200

    Renat double_linked_list

--- OneLinkedList/dll_node.h
+++ OneLinkedList/dll_node.h
@@ -1,34 +1,39 @@
 #pragma once
-
 #include <string>
 #include <sstream>
-#include "Book.h"
 
-namespace oll
+namespace dll
 {
 	template<class T>
 	class Node
 	{
 	public:
-		Node(T data, Node<T>* next = nullptr);
+		Node(T data, Node<T>* next = nullptr, Node<T>* prev = nullptr);
 
 		Node<T>*& Next() { return next; }
 		const Node<T>* Next() const { return next; }
+
+		Node<T>*& Prev() { return prev; }
+		const Node<T>* Prev() const { return prev; }
 
 		T& Data() { return data; }
 		const T& Data() const { return data; }
 
 		bool IsLast() const { return next == nullptr; }
+		bool IsFirst() const { return prev == nullptr; }
 		std::string ToString() const;
+		std::string ToStringR() const;
 	private:
 		Node<T>* next = nullptr;
+		Node<T>* prev = nullptr;
 		T data = 0;
 	};
 
 	template<class T>
-	Node<T>::Node(T _data, Node<T>* _next)
+	Node<T>::Node(T _data, Node<T>* _next, Node<T>* _prev,)
 		: data(_data)
 		, next(_next)
+		, prev(_prev)
 	{}
 
 	template<class T>
@@ -43,4 +48,16 @@ namespace oll
 		return oss.str();
 	}
 
-} //namespace oll
+	template<class T>
+	std::string Node<T>::ToStringR() const
+	{
+		std::ostringstream oss;
+		oss << data << " ";
+		if (!IsFirst())
+		{
+			oss << prev->ToStringR();
+		}
+		return oss.str();
+	}
+
+} // namespace dll
--- OneLinkedList/one_linked_list.h
+++ OneLinkedList/one_linked_list.h
@@ -3,171 +3,176 @@
 #include <string>
 #include "node.h"
 
-template <class T>
-class OneLinkedList
+namespace oll
 {
-public:
-	size_t Size() const { return size; }
-	std::string ToString() const;
 
-	void push_front(T val);
-	void push_back(T val);
-	void push_mid(T val, size_t pos);
+	template <class T>
+	class OneLinkedList
+	{
+	public:
+		size_t Size() const { return size; }
+		std::string ToString() const;
 
-	void pop_front();
-	void pop_back();
-	void pop_mid(size_t pos);
-private:
-	Node<T>* head = nullptr;
-	Node<T>* tail = nullptr;
-	size_t size = 0;
-};
+		void push_front(T val);
+		void push_back(T val);
+		void push_mid(T val, size_t pos);
 
-template <class T>
-std::string OneLinkedList<T>::ToString() const
-{
-	std::ostringstream oss;
-	oss << size << ":[";
-	if (head)
-	{
-		oss << head->ToString();
-	}
-	oss << "]";
-	return oss.str();
-}
+		void pop_front();
+		void pop_back();
+		void pop_mid(size_t pos);
+	private:
+		Node<T>* head = nullptr;
+		Node<T>* tail = nullptr;
+		size_t size = 0;
+	};
 
-template <class T>
-void OneLinkedList<T>::push_front(T val)
-{
-	if (size == 0)
+	template <class T>
+	std::string OneLinkedList<T>::ToString() const
 	{
-		head = new Node<T>(val);
-		tail = head;
-	}
-	else
-	{
-		head = new Node<T>(val, head);
-	}
-	size++;
-}
-
-template <class T>
-void OneLinkedList<T>::push_back(T val)
-{
-	if (size == 0)
-	{
-		head = new Node<T>(val);
-		tail = head;
-	}
-	else
-	{
-		tail->Next() = new Node<T>(val);
-		tail = tail->Next();
-	}
-	size++;
-}
-
-template <class T>
-void OneLinkedList<T>::push_mid(T val, size_t pos)
-{
-	if (pos == 0)
-	{
-		push_front(val);
-		return;
-	}
-	if (pos > Size())
-	{
-		push_back(val);
-		return;
-	}
-
-	Node<T>* t = head;
-	size_t k = 0;
-	while (k != pos - 1)
-	{
-		t = t->Next();
-		k++;
-	}
-	Node<T>* t1 = t->Next();
-	t->Next() = new Node<T>(val, t1);
-	size++;
-}
-
-template <class T>
-void OneLinkedList<T>::pop_front()
-{
-	if (size == 0)
-		return;
-	if (size == 1)
-	{
-		delete head;
-		head = tail = nullptr;
-	}
-	else
-	{
-		Node<T>* t = head;
-		head = head->Next();
-		t->Next() = nullptr;
-		delete t;
-	}
-	size--;
-}
-
-template <class T>
-void OneLinkedList<T>::pop_back()
-{
-	if (size == 0)
-		return;
-	if (size == 1)
-	{
-		delete head;
-		head = tail = nullptr;
-	}
-	else
-	{
-		Node<T>* t = head;
-		while (t->Next() != tail)
+		std::ostringstream oss;
+		oss << size << ":[";
+		if (head)
 		{
-			t = t->Next();
+			oss << head->ToString();
+		}
+		oss << "]";
+		return oss.str();
+	}
+
+	template <class T>
+	void OneLinkedList<T>::push_front(T val)
+	{
+		if (size == 0)
+		{
+			head = new Node<T>(val);
+			tail = head;
+		}
+		else
+		{
+			head = new Node<T>(val, head);
+		}
+		size++;
+	}
+
+	template <class T>
+	void OneLinkedList<T>::push_back(T val)
+	{
+		if (size == 0)
+		{
+			head = new Node<T>(val);
+			tail = head;
+		}
+		else
+		{
+			tail->Next() = new Node<T>(val);
+			tail = tail->Next();
+		}
+		size++;
+	}
+
+	template <class T>
+	void OneLinkedList<T>::push_mid(T val, size_t pos)
+	{
+		if (pos == 0)
+		{
+			push_front(val);
+			return;
+		}
+		if (pos > Size())
+		{
+			push_back(val);
+			return;
 		}
 
-		t->Next() = nullptr;
-		delete tail;
-		tail = t;
-	}
-	size--;
-}
-
-template <class T>
-void OneLinkedList<T>::pop_mid(size_t pos)
-{
-	if (size == 0)
-		return;
-	if (pos == 0)
-	{
-		pop_front();
-		return;
-	}
-	if (pos >= size)
-	{
-		pop_back();
-		return;
+		Node<T>* t = head;
+		size_t k = 0;
+		while (k != pos - 1)
+		{
+			t = t->Next();
+			k++;
+		}
+		Node<T>* t1 = t->Next();
+		t->Next() = new Node<T>(val, t1);
+		size++;
 	}
 
-	Node<T>* t = head;
-	size_t k = 0;
-	while (k != pos - 1)
+	template <class T>
+	void OneLinkedList<T>::pop_front()
 	{
-		t = t->Next();
-		k++;
+		if (size == 0)
+			return;
+		if (size == 1)
+		{
+			delete head;
+			head = tail = nullptr;
+		}
+		else
+		{
+			Node<T>* t = head;
+			head = head->Next();
+			t->Next() = nullptr;
+			delete t;
+		}
+		size--;
 	}
-	Node<T>* del = t->Next();
-	if (del == tail)
+
+	template <class T>
+	void OneLinkedList<T>::pop_back()
 	{
-		tail = t;
+		if (size == 0)
+			return;
+		if (size == 1)
+		{
+			delete head;
+			head = tail = nullptr;
+		}
+		else
+		{
+			Node<T>* t = head;
+			while (t->Next() != tail)
+			{
+				t = t->Next();
+			}
+
+			t->Next() = nullptr;
+			delete tail;
+			tail = t;
+		}
+		size--;
 	}
-	t->Next() = del->Next();
-	del->Next() = nullptr;
-	delete del;
-	size--;
-}
+
+	template <class T>
+	void OneLinkedList<T>::pop_mid(size_t pos)
+	{
+		if (size == 0)
+			return;
+		if (pos == 0)
+		{
+			pop_front();
+			return;
+		}
+		if (pos >= size)
+		{
+			pop_back();
+			return;
+		}
+
+		Node<T>* t = head;
+		size_t k = 0;
+		while (k != pos - 1)
+		{
+			t = t->Next();
+			k++;
+		}
+		Node<T>* del = t->Next();
+		if (del == tail)
+		{
+			tail = t;
+		}
+		t->Next() = del->Next();
+		del->Next() = nullptr;
+		delete del;
+		size--;
+
+	}
+} //namespace oll
