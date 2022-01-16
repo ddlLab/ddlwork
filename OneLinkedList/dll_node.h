316bbcc12aba16be6c87260f1952d8ca88d794ea
ddlLab <ddl.rybchenko@gmail.com>
2022-01-16 13:04:23 +0200

    *

--- OneLinkedList/dll_node.h
+++ OneLinkedList/dll_node.h
@@ -10,10 +10,15 @@ template<class T>
 class Node
 {
 public:
-	Node(T data, Node<T>* next = nullptr);
+	Node(T data,
+		 Node<T>* next = nullptr,
+		 Node<T>* prev = nullptr);
 
 	Node<T>*& Next() { return next; }
 	const Node<T>* Next() const { return next; }
+
+	Node<T>*& Prev() { return prev; }
+	const Node<T>* Prev() const { return prev; }
 
 	T& Data() { return data; }
 	const T& Data() const { return data; }
@@ -27,5 +32,36 @@ private:
 	Node<T>* prev = nullptr;
 	T data = 0;
 };
+
+template<class T>
+Node<T>::Node(T _data, Node<T>* _next, Node<T>* _prev)
+: data(_data)
+, next(_next)
+, prev(_prev)
+{}
+
+template<class T>
+std::string Node<T>::ToString() const
+{
+	std::ostringstream oss;
+	oss << data << " ";
+	if (!IsLast())
+	{
+		oss << next->ToString();
+	}
+	return oss.str();
+}
+
+template<class T>
+std::string Node<T>::ToStringR() const
+{
+	std::ostringstream oss;
+	oss << data << " ";
+	if (!IsFirst())
+	{
+		oss << prev->ToString();
+	}
+	return oss.str();
+}
 
 } // namespace dll
