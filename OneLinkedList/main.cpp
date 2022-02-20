88cd7cfb44212d895fac2da363ae72fb209e9e3c
ddlLab <ddl.rybchenko@gmail.com>
2022-02-20 12:11:41 +0200

    *

--- OneLinkedList/main.cpp
+++ OneLinkedList/main.cpp
@@ -4,6 +4,7 @@
 #include "queue.h"
 #include "stack.h"
 #include "set1.h"
+#include "tree.h"
 #include "Book.h"
 
 void test1();
@@ -16,6 +17,7 @@ void test7();
 void test8();
 void test9();
 void test10();
+void test11();
 
 int main()
 {
@@ -28,7 +30,8 @@ int main()
     //test7();
     //test8();
     //test9();
-    test10();
+    //test10();
+    test11();
     return 0;
 }
 
@@ -255,4 +258,22 @@ void test10()
         std::cout << st1.ToString() << std::endl;
         st1.pop();
     }
+}
+
+void test11()
+{
+    tree::Tree<int> tr1;
+    tr1.Add(45);
+    tr1.Add(45);
+    tr1.Add(25);
+    tr1.Add(35);
+    tr1.Add(65);
+    tr1.Add(55);
+    tr1.Add(75);
+    tr1.Add(15);
+    tr1.Add(27);
+    tr1.Add(48);
+    std::cout<<tr1.ToString() << std::endl;
+    std::cout << tr1.Has(25) << std::endl;
+    std::cout << tr1.Has(21) << std::endl;
 }
